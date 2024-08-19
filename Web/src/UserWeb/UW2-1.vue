cb103e3849d72527fd372f7897f99f81911d21d5
zhan <2353215508@qq.com>
2024-08-19 10:26:45 +0800

    提交

--- Web/src/UserWeb/UW2-1.vue
+++ Web/src/UserWeb/UW2-1.vue
@@ -7,7 +7,12 @@
         </div>
         <div style="margin-top: 10px;margin-left: 10px;margin-right: 10px; width: 100%;height: 80px;">
             <el-card>
-                <h2>基本信息</h2>
+                <h3>基本信息</h3>
+                <span>姓名：{{ name }}</span>
+                <span>性别：{{ gender }}</span>
+                <span>年龄：{{ age }}</span>
+                <span>邮箱：{{ email }}</span>
+                <span><img src="" alt="用户图片"></span>
             </el-card>
         </div>
     </div>
