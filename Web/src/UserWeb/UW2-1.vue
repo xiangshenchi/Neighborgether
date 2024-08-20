b3c85c36908adff1be71a960ca8ce95b177bb9c0
mingtian2004 <3203644074@qq.com>
2024-08-20 15:37:21 +0800

    业主个人界面更新

--- Web/src/UserWeb/UW2-1.vue
+++ Web/src/UserWeb/UW2-1.vue
@@ -12,8 +12,14 @@
                 <span>性别：{{ gender }}</span>
                 <span>年龄：{{ age }}</span>
                 <span>邮箱：{{ email }}</span>
+               
+            </el-card>
+        </div>
+        <div style="margin-top: 100px;margin-left: 10px;margin-right: 10px; width: 100%;height: 80px;">
+            <el-card>
                 <span><img src="" alt="用户图片"></span>
             </el-card>
+            
         </div>
     </div>
 </template>
