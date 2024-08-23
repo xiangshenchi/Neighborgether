9ec9f24049eb76cbd65cb3eb991d7d6d9ad3423b
shawn <3057098742@qq.com>
2024-08-23 16:20:49 +0800

    “楼栋管理”功能去除

--- Web/src/admin/AM.vue
+++ Web/src/admin/AM.vue
@@ -63,7 +63,7 @@
             <el-menu-item index="/AM/AW3-1">公告管理</el-menu-item>
             <el-menu-item index="/AM/AW3-2">车辆管理</el-menu-item>
             <el-menu-item index="/AM/AW3-3">房产管理</el-menu-item>
-            <el-menu-item index="/AM/AW3-4">楼栋管理</el-menu-item>
+            <!-- <el-menu-item index="/AM/AW3-4">楼栋管理</el-menu-item> -->
             <el-menu-item index="/AM/AW3-5">访客管理</el-menu-item>
             <!-- <el-menu-item index="/AM/AW3-6">工作分配</el-menu-item> -->
           </el-sub-menu>
