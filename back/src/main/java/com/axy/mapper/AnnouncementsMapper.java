f9501a6f9f12dc9977232e92f8f25a2921bda7b6
mingtian2004 <3203644074@qq.com>
2024-08-23 15:57:58 +0800

    Demand,Announcements后端更新

--- back/src/main/java/com/axy/mapper/AnnouncementsMapper.java
+++ back/src/main/java/com/axy/mapper/AnnouncementsMapper.java
@@ -2,7 +2,15 @@ package com.axy.mapper;
 
 import com.axy.pojo.Announcements;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
 * @author DELL
@@ -13,6 +21,12 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface AnnouncementsMapper extends BaseMapper<Announcements> {
 
+    //展示公告
+     List<Announcements> list();
+    //增加公告
+    boolean add(@RequestBody Announcements announcements);
+    //删除公告
+    boolean delete(@RequestParam("announcementid") int announcementid);
 }
 
 
