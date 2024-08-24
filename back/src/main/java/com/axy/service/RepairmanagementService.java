5cb834dbdacd427e947f4cdd9ed2f17a61d3f1c1
karenfeng22 <986437891@qq.com>
2024-08-24 11:16:18 +0800

    35

---
+++ back/src/main/java/com/axy/service/RepairmanagementService.java
@@ -0,0 +1,13 @@
+package com.axy.service;
+
+import com.axy.pojo.Repairmanagement;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+* @author DELL
+* @description 针对表【repairmanagement】的数据库操作Service
+* @createDate 2024-08-24 09:24:53
+*/
+public interface RepairmanagementService extends IService<Repairmanagement> {
+
+}
