f69f49b7219bfc870085b9a60be3ee21beac89ae
karenfeng22 <986437891@qq.com>
2024-08-24 17:32:42 +0800

    35

--- back/src/main/java/com/axy/controller/LivingpaymentController.java
+++ back/src/main/java/com/axy/controller/LivingpaymentController.java
@@ -4,13 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.axy.common.LupdateRequest;
 import com.axy.mapper.LivingpaymentMapper;
@@ -69,4 +63,10 @@ public class LivingpaymentController {
 
         return response;
     }
+    @DeleteMapping("/delete")
+    //管理员删除缴费信息
+    public boolean delete(@RequestParam int paymentid) {
+        return liv.removeById(paymentid);
+    }
+    //管理员添加缴费信息
 }
